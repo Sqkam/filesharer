1e4ab5998d7de4a686b8097789be114de228da4c
Sqkam <paopaosqkam@icloud.com>
2024-08-02 01:27:19 +0800

    优化代码

--- internal/service/file.go
+++ internal/service/file.go
@@ -2,13 +2,10 @@ package service
 
 import (
 	"context"
-	"errors"
 	pb "filesharer/api/file/v1"
 	v1 "filesharer/api/file/v1"
 	"filesharer/internal/biz"
 	"filesharer/internal/data"
-	"fmt"
-	"github.com/todocoder/go-stream/stream"
 	"net/url"
 	"sync"
 )
@@ -70,70 +67,10 @@ func (s *FileService) GetDetailByAddr(ctx context.Context, req *pb.GetDetailByAd
 	return resp, err
 }
 func (s *FileService) DownloadByAddr(req *pb.DownloadByAddrRequest, conn pb.File_DownloadByAddrServer) error {
-	// 不会下载自己实例的文件
-	node, err := s.uc.ListNode(context.Background(), &pb.ListNodeRequest{})
-	if err != nil {
-		return err
-	}
-	noMatch := stream.Of(node.Data...).NoneMatch(func(item *v1.ListNodeReplyItem) bool {
-		return fmt.Sprintf("%s:%d", item.ServiceAddress, item.ServicePort) == req.Addr
-	})
-	if noMatch {
-		return errors.New("非法addr")
-	}
-
-	if Endpoint.Host == req.Addr {
-		return s.uc.DownloadByAddr(req, conn)
-	}
-
-	client, err := s.getClient(req.Addr)
-	if err != nil {
-		return err
-	}
-	stream, err := client.DownloadByAddr(context.Background(), req)
-	if err != nil {
-		m.Delete(req.Addr)
-		return err
-	}
-	err = s.uc.DownloadByStream(stream, req.Path)
-	if err != nil {
-		return err
-	}
-	return nil
-
+	return s.uc.DownloadByAddr(req, conn)
 }
 func (s *FileService) DownloadDirByAddr(req *pb.DownloadDirByAddrRequest, conn pb.File_DownloadDirByAddrServer) error {
-	// 不会下载自己实例的文件
-	node, err := s.uc.ListNode(context.Background(), &pb.ListNodeRequest{})
-	if err != nil {
-		return err
-	}
-	noMatch := stream.Of(node.Data...).NoneMatch(func(item *v1.ListNodeReplyItem) bool {
-		return fmt.Sprintf("%s:%d", item.ServiceAddress, item.ServicePort) == req.Addr
-	})
-	if noMatch {
-		return errors.New("非法addr")
-	}
-
-	if Endpoint.Host == req.Addr {
-		return s.uc.DownloadDirByAddr(req, conn)
-	}
-
-	client, err := s.getClient(req.Addr)
-	if err != nil {
-		return err
-	}
-	stream, err := client.DownloadDirByAddr(context.Background(), req)
-	if err != nil {
-		m.Delete(req.Addr)
-		return err
-	}
-	err = s.uc.DownloadDirByStream(stream, req.Path)
-	if err != nil {
-		return err
-	}
-	return nil
-
+	return s.uc.DownloadDirByAddr(req, conn)
 }
 
 func (s *FileService) ListNode(ctx context.Context, req *pb.ListNodeRequest) (*pb.ListNodeReply, error) {
